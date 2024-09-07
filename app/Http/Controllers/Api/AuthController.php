6054a7c28b4186f256e4f8494a727c0f2ffddde8
taroosg <root@taroosg.dev>
2024-09-07 11:20:26 +0900

    ユーザログインAPIとログアウトAPI追加

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -31,4 +31,34 @@ class AuthController extends Controller
       'token_type' => 'Bearer',
     ]);
   }
+
+  public function login(Request $request)
+  {
+    $request->validate([
+      'email' => 'required|email',
+      'password' => 'required',
+    ]);
+
+    if (!Auth::attempt($request->only('email', 'password'))) {
+      return response()->json([
+        'message' => 'Invalid login details'
+      ], 401);
+    }
+
+    $user = User::where('email', $request['email'])->firstOrFail();
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+      'access_token' => $token,
+      'token_type' => 'Bearer',
+    ]);
+  }
+
+  public function logout(Request $request)
+  {
+    $request->user()->currentAccessToken()->delete();
+
+    return response()->json(['message' => 'Successfully logged out']);
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/user', function (Request $request) {
   return $request->user();
