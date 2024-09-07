64e5facc7d20adad96dc1b87875f25b771b89bee
taroosg <root@taroosg.dev>
2024-09-07 11:18:44 +0900

    ユーザ登録API追加

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Auth;
+
+class AuthController extends Controller
+{
+  public function register(Request $request)
+  {
+    $validatedData = $request->validate([
+      'name' => 'required|string|max:255',
+      'email' => 'required|string|email|max:255|unique:users',
+      'password' => 'required|string|min:8',
+    ]);
+
+    $user = User::create([
+      'name' => $validatedData['name'],
+      'email' => $validatedData['email'],
+      'password' => Hash::make($validatedData['password']),
+    ]);
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+      'access_token' => $token,
+      'token_type' => 'Bearer',
+    ]);
+  }
+}
